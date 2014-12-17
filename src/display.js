1699824d6ddd2a6ada07529fb49bc7b76c3e2bba
Pascal Duez <pascal@duez.me>
2014-12-17 11:22:26 +0100

    Ensure `display.access` is defined before testing - Refs SassDoc/sassdoc-theme-default#27

--- src/display.js
+++ src/display.js
@@ -1,12 +1,12 @@
 'use strict';
 
 /**
- * Compute a `display` property regarding of access display
+ * Compute a `display` property in regards of `display.access`
  * configuration.
  */
 module.exports = function display(ctx) {
   ctx.data = ctx.data.filter(function (item) {
-    var displayItemAccess = ctx.display.access.indexOf(item.access) !== -1;
+    var displayItemAccess = ctx.display.access ? (ctx.display.access.indexOf(item.access) !== -1) : false;
     var isAlias = item.alias;
     var displayAlias = ctx.display.alias;
 
